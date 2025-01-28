ca8b69218000a4af1a68638039cd93c393822549
a_sabaleuski <acidflag@gmail.com>
2025-01-28 22:07:05 +0300

    12.8.6

--- products/models.py
+++ products/models.py
@@ -84,8 +84,8 @@ class Drawing(models.Model):
         verbose_name="Родительский чертеж"
     )
 
-    doc_name = models.CharField(max_length=255, null=False, blank=False, default="Untitled")
-    mass = models.FloatField()
+    doc_name = models.CharField(max_length=255, null=False, blank=False, default="Untitled", unique=True)
+    mass = models.FloatField(null=True, blank=True)
     assembly_unit = models.BooleanField(default=False, verbose_name="СБ")
 
     def clean(self):
